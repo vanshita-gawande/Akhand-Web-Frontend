d3f3459f7af032b9ac1bc2ee2566a6c9a2f7bda3
vanshita-gawande <vanshitapatil01@gmail.com>
2025-09-17 14:56:51 +0530

    Initial Commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,26 +4,11 @@ import AboutUs from "./AboutUs";
 import Services from "./Services";
 import VenuesSection from "./VenuesSection";
 import RegisterPromptModal from "./popups/RegisterPromptPopup";
-import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 export default function Dashboard({ onLoginClick, onRegisterClick }) {
   const companyName = "Akhand Sports";
   const [showModal, setShowModal] = useState(false);
-  const navigate = useNavigate();
-
-
-  // const handleRegisterClick = () => {
-  //   const token = localStorage.getItem("token");
-
-  //   if (!token) {
-  //     setShowModal(true); // open popup
-  //     return;
-  //   }
-  //   console.log("Proceed with booking… user is logged in ✅");
-  //     // ✅ user is logged in, redirect to UserDashboard
-  // navigate("/userdashboard");
-  // };
 
   return (
     <main className="max-w-[1200px] mx-auto p-6 md:p-10 space-y-12 mt-6">
